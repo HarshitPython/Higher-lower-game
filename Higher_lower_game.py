79027d09bcf6e27638a74e18534a62c09653cada
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-20 17:55:35 +0530

    Update Higher_lower_game.py

--- Higher_lower_game.py
+++ Higher_lower_game.py
@@ -24,6 +24,8 @@ def check_answer(guess, a_followers, b_followers):
   """Checks followers against user's guess 
   and returns True if they got it right.
   Or False if they got it wrong.""" 
+  
+  # If Statement
   if a_followers > b_followers:
     return guess == "a"
   else:
